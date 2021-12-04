5c6199008dd3baeb4af6a92f4edf5628424426be
bafroda <94061662+bafroda@users.noreply.github.com>
2021-12-04 18:02:02 +0100

    Date on button click

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 var myPage = document.querySelector('h1');
-const WilDE = 'Willkommen';
-const WelEN = 'Welcome';
 
 myPage.onclick = function() {
+    let WilDE = 'Willkommen';
+    let WelEN = 'Welcome';
+
     if (myPage.textContent === WilDE) {
         myPage.textContent = WelEN;
     } else {
         myPage.textContent = WilDE;
     }
 
+    var My123 = WilDE;
+    console.log(My123);
+    var My456 = WelEN;
+    console.log(My456);
+}
+
+var MyButton = document.querySelector('button');
+
+MyButton.onclick = function() {
+    var myP = document.querySelector('p');
+    var AktuellesDatum = Date();
+    myP.textContent = AktuellesDatum;
 }
